c1548ed5812803b68fb5b513b8e2012e7957fb45
Nikita <niker217@mail.ru>
2023-09-21 14:18:30 +0700

    -добавлены TODO и README

---
+++ Project/MDK-ARM/hdlc_autorization.d
@@ -0,0 +1,2 @@
+.\hdlc_autorization.o: HDLC_Autorization.c
+.\hdlc_autorization.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ Project/MDK-ARM/protocol.d
@@ -0,0 +1,4 @@
+.\protocol.o: C:\Programing\HDLC_STM32\Project\MDK-ARM\HDLC_Protocol\Protocol.c
+.\protocol.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\protocol.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\protocol.o: C:\Programing\HDLC_STM32\Project\MDK-ARM\HDLC_Protocol\Protocol.h
